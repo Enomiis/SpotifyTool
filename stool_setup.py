a8a2bc109a1321379c623970054dab031ed62809
Im Enomis <enomis352@gmail.com>
2022-12-09 13:03:07 +0100

    setup done

--- stool_setup.py
+++ stool_setup.py
@@ -11,6 +11,7 @@ def typewrite(text):
     pyautogui.hotkey('ctrl', 'v')
     pyperclip.copy('')
 
+pyautogui.FAILSAFE = False
 bakkesmod_directory = input("Directory to BakkesMod folder? ")
 web_browser = input("Which web browser are you using? ")
 web_browser = web_browser.lower()
@@ -26,6 +27,7 @@ webbrowser.open(link)
 while not "localhost" in str(os.popen("tasklist /v /fi \"IMAGENAME eq " + str(web_browser) + ".exe\" /fo \"list\" | find \"Titre de la fenêtre:\"").read()):
     pass
 time.sleep(5)
+code = ""
 pyautogui.keyDown("ctrl")
 pyautogui.keyDown("shift")
 pyautogui.keyDown("j")
@@ -36,17 +38,23 @@ time.sleep(2)
 typewrite('document.title=pageData.reloadButton.reloadUrl.split("code=")[1]')
 pyautogui.press("enter")
 time.sleep(2)
+url_containing_code = (str(os.popen("tasklist /v /fi \"IMAGENAME eq " + str(web_browser) + ".exe\" /fo \"list\" | find \"Titre de la fenêtre:\"").read()).split("\n")[0])
+code += url_containing_code.split(": ")[-1]
+typewrite('document.title=document.title.substring(255)')
+pyautogui.press("enter")
+time.sleep(2)
+url_containing_code = (str(os.popen("tasklist /v /fi \"IMAGENAME eq " + str(web_browser) + ".exe\" /fo \"list\" | find \"Titre de la fenêtre:\"").read()).split("\n")[0])
+index = (url_containing_code.split(": ")[-1]).find(" - ")
+code += (url_containing_code.split(": ")[-1])[:-index+4]
 pyautogui.keyDown("ctrl")
 pyautogui.keyDown("shift")
 pyautogui.keyDown("j")
 pyautogui.keyUp('j')
 pyautogui.keyUp('shift')
 pyautogui.keyUp('ctrl')
-url_containing_code = (str(os.popen("tasklist /v /fi \"IMAGENAME eq " + str(web_browser) + ".exe\" /fo \"list\" | find \"Titre de la fenêtre:\"").read()).split("\n")[0])
 pyautogui.keyDown('ctrl')
 pyautogui.press('w')
 pyautogui.keyUp('ctrl')
-code = url_containing_code.split(": ")[-1]
 default_config = {
     "access_token":"",
     "artist":"",
